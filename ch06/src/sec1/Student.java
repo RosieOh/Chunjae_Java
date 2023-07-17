787e021790e74bb71ed947e6c7deba465a57cc4f
RosieOh <20172207@gm.hannam.ac.kr>
2023-07-17 14:43:55 +0900

    테스트

---
+++ ch06/src/sec1/Student.java
@@ -0,0 +1,15 @@
+package sec1;
+
+public class Student {
+    private int no;
+    private String name;
+    private int kor;
+    private int eng;
+    private int mat;
+
+    // 접근제한자 반환타입 메서드명([타입 매개변수
+    // 1. 리턴X, 매개변수X
+    public void  method1() {
+        System.out.println("메서드1");
+    }
+}
